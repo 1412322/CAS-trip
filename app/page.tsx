d8bc728cc5c4f77c16cad5897f9c85651d241b61
Minh Luong <nhatminh190496@gmail.com>
2025-09-27 05:14:02 +0700

    chore: refactor

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,6 @@ import Partners from './Partners'
 import Travelers from './Travelers'
 
 export default function Home() {
-  return <div>maintenance</div>
   return (
     <main>
       {/* Hero Section */}
